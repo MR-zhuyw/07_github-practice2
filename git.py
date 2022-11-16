852e04600fa85626ab05909ec82becba9b449edb
zhu <1596992084@qq.com>
2022-11-16 11:22:00 +0800

    commit1

---
+++ git.py
@@ -0,0 +1,7 @@
+print("hello git")
+print("hello git again")
+print("hello git again and again")
+print("hello git hot-fix")
+print("hello git master-test")
+print("hello git hot-fix2")
+print("push test")
